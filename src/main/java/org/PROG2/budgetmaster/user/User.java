badde7a786b60ca1568f0e4c339cbc73ad3585a5
Phylosius <hei.phylosius@gmail.com>
2024-08-21 11:08:33 +0300

    feat: added expenses addition and sorting to User

--- src/main/java/org/PROG2/budgetmaster/user/User.java
+++ src/main/java/org/PROG2/budgetmaster/user/User.java
@@ -3,6 +3,7 @@ package org.PROG2.budgetmaster.user;
 import lombok.Getter;
 import lombok.Setter;
 import org.PROG2.budgetmaster.expense.Expense;
+import org.PROG2.budgetmaster.expense.ExpenseCategory;
 import org.PROG2.budgetmaster.utils.Generator;
 
 import java.util.List;
@@ -20,5 +21,20 @@ public class User {
         this.name = name;
         this.monthlyBudget = monthlyBudget;
 
+    }
+
+    public void addExpense(String description, double amount, ExpenseCategory category, String date) {
+        expenses.add(new Expense(description, amount, category, date));
+    }
+
+    public List<Expense> getExpensesDateSorted() {
+        // return expenses sort by date
+        expenses.sort((exp1, exp2) -> exp2.getDate().compareTo(exp1.getDate()));
+        return expenses;
+    }
+
+    public List<Expense> getExpensesCategorySorted() {
+        expenses.sort((exp1, exp2) -> exp2.getCategory().compareTo(exp1.getCategory()));
+        return expenses;
     }
 }
